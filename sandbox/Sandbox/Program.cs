de21b1c7e0eac714110ff7d2d836467ef0ad54cf
styvejuma <stephanejuma1@gmail.com>
2025-01-17 18:38:45 +0200

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello what is your first name?.");
+        Console.WriteLine("Hello what is your Last name?.");
     }
 }
